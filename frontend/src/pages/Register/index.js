77f25122c31be23a887f2855639eb993d9d04beb
Larissa Tauana <laritauana@users.noreply.github.com>
2020-04-20 17:28:37 -0300

    Front-end e back-end da aplicação

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -34,7 +34,7 @@ export default function Register() {
         
         history.push('/');
     } catch (err) {
-        alert('Erro no cadastro, tente novamente.');
+        alert(`Erro ao cadastrar, tente novamente.`);
     }  
   }
 
